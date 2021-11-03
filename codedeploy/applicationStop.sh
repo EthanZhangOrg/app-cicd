885397e9cc1d57ad023de4f84dcc45a5ae3dc7a5
Ethan Zhang <ethanzhang1997@gmail.com>
2021-11-03 17:54:59 -0400

    1

--- codedeploy/applicationStop.sh
+++ codedeploy/applicationStop.sh
@@ -2,13 +2,4 @@
 
 # stop the application
 pid=$(sudo lsof -i tcp:8081 -t)
-echo $pid
-
-if test -z $pid
-then
-    echo "Program is not running!"
-else
-    echo "Program is running!"
-    echo $pid
-    sudo kill -9 $pid
-fi
+sudo kill -9 $pid || echo "No process run on 8081"
