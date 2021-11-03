852ffc383244d5e1eda1b1876e029424ca9cdc8e
Ethan Zhang <ethanzhang1997@gmail.com>
2021-11-03 18:55:37 -0400

    update scripts for deploy

--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # start the application
-sudo java -jar /home/ubuntu/webapp_jar/webapp-0.0.1-SNAPSHOT.jar --server.port=8081 --spring.config.location=/home/ubuntu/webapp_jar/application.properties &
+sudo nohup java -jar /home/ubuntu/webapp_jar/webapp-0.0.1-SNAPSHOT.jar --server.port=8081 --spring.config.location=/home/ubuntu/webapp_jar/application.properties &
